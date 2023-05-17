42b4178005fc6dd86b4483155093d02bd2799936
this-is-yello <yello_3617@naver.com>
2023-05-17 19:06:34 +0900

    in C

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:opscroll_web/opscroll_web.dart';
-import 'package:flutter_carousel_slider/carousel_slider.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,121 +13,6 @@ class MyApp extends StatelessWidget {
       title: '김주현바이각',
       debugShowCheckedModeBanner: false,
       home: MainPage(),
-    );
-  }
-}
-
-class MainPage extends StatefulWidget {
-  MainPage({super.key});
-
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  final PageController _pageController = PageController();
-
-  int _currentPage = 0;
-  List<String> mainBackground = [
-    'assets/images/tailorShop_bg.png',
-    'assets/images/tailorAcademy_bg.png',
-    'assets/images/jemulpoClub_bg.png',
-    'assets/images/rentalCenter_bg.png',
-  ];
-  List<String> mainTitles = [
-    'k!mjuhyeon by 覺',
-    'by 覺 Tailor Academy',
-    'New JemulpoClub',
-    'by 覺 RentalCenter',
-  ];
-  List<String> mainSubTitles = [
-    '전통과 정통을 바느질하다.',
-    '어제보다 나은 작업물을 만드는 것이 이 시대의 장인정신입니다.',
-    '개화기 제물포구락부의 역사를 계승하고자 합니다.',
-    '그 날을 위한 자신감, 바이각',
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        alignment: Alignment.topCenter,
-        children: [
-          OpscrollWeb(
-            pageController: _pageController,
-            onePageChildren: [
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                child: CarouselSlider.builder(
-                  slideBuilder: (index) {
-                    return Container(
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: AssetImage(
-                            mainBackground[index],
-                          ),
-                        ),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            mainTitles[index],
-                            style: TextStyle(
-                              fontSize: 56,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                          Text(
-                            mainSubTitles[index],
-                            style: TextStyle(
-                              fontSize: 24,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                  slideTransform: DefaultTransform(),
-                  slideIndicator: CircularSlideIndicator(
-                    currentIndicatorColor: Colors.white,
-                    padding: EdgeInsets.only(bottom: 80),
-                  ),
-                  unlimitedMode: true,
-                  itemCount: mainBackground.length,
-                ),
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: 200,
-                color: Colors.red,
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                color: Colors.green,
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                color: Colors.blue,
-              ),
-            ],
-            scrollCurve: Curves.easeIn,
-            scrollSpeed: const Duration(milliseconds: 1000),
-            isFloatingButtonActive: false,
-            // isTouchScrollingActive: false,
-            scrollingAnimationOptions: ScrollingAnimationOptions.Default,
-          ),
-          MainAppBar(),
-        ],
-      ),
     );
   }
 }
@@ -241,21 +126,21 @@ class MainAppBar extends StatelessWidget {
           Container(
             child: Row(
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(
-                    right: 20,
-                  ),
-                  child: InkWell(
-                    child: Text(
-                      'KOR',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                      ),
-                    ),
-                    onTap: () {},
-                  ),
-                ),
+                // Padding(
+                //   padding: const EdgeInsets.only(
+                //     right: 20,
+                //   ),
+                //   child: InkWell(
+                //     child: Text(
+                //       'KOR',
+                //       style: TextStyle(
+                //         fontSize: 20,
+                //         color: Colors.white,
+                //       ),
+                //     ),
+                //     onTap: () {},
+                //   ),
+                // ),
                 Padding(
                   padding: const EdgeInsets.only(
                     left: 20,
@@ -275,6 +160,295 @@ class MainAppBar extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  MainPage({super.key});
+
+  final PageController _pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          OpscrollWeb(
+            pageController: _pageController,
+            onePageChildren: [
+              CarouselScreen(),
+              BykakStory(),
+              TailorShopScreen(),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                color: Colors.green,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                color: Colors.blue,
+              ),
+            ],
+            scrollCurve: Curves.easeIn,
+            scrollSpeed: const Duration(milliseconds: 1000),
+            isFloatingButtonActive: false,
+            scrollingAnimationOptions: ScrollingAnimationOptions.Default,
+          ),
+          MainAppBar(),
+        ],
+      ),
+    );
+  }
+}
+
+class CarouselScreen extends StatefulWidget {
+  const CarouselScreen({super.key});
+
+  @override
+  State<CarouselScreen> createState() => _CarouselScreenState();
+}
+
+class _CarouselScreenState extends State<CarouselScreen> {
+  int _currentPage = 0;
+  List<String> mainBackground = [
+    'assets/images/tailorShop_bg.png',
+    'assets/images/tailorAcademy_bg.png',
+    'assets/images/jemulpoClub_bg.png',
+    'assets/images/rentalCenter_bg.png',
+  ];
+  List<String> mainTitles = [
+    'k!mjuhyeon by 覺',
+    'by 覺 Tailor Academy',
+    'New JemulpoClub',
+    'by 覺 RentalCenter',
+  ];
+  List<String> mainSubTitles = [
+    '전통과 정통을 바느질하다.',
+    '어제보다 나은 작업물을 만드는 것이 이 시대의 장인정신입니다.',
+    '개화기 제물포구락부의 역사를 계승하고자 합니다.',
+    '그 날을 위한 자신감, 바이각',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: CarouselSlider.builder(
+        options: CarouselOptions(
+          viewportFraction: 1,
+          autoPlay: true,
+          autoPlayInterval: Duration(milliseconds: 7000),
+          autoPlayAnimationDuration: Duration(milliseconds: 1500),
+          autoPlayCurve: Curves.fastOutSlowIn,
+          onPageChanged: (index, reason) {
+            _currentPage = index;
+            print(index);
+          },
+        ),
+        itemBuilder: (context, index, realIndex) {
+          return Container(
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage(
+                  mainBackground[index],
+                ),
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  mainTitles[index],
+                  style: TextStyle(
+                    fontSize: 64,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 8,
+                    bottom: 20,
+                  ),
+                  child: Text(
+                    mainSubTitles[index],
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    width: 80,
+                    height: 80,
+                    child: Center(
+                      child: Text(
+                        'view',
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(500),
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+        itemCount: mainBackground.length,
+      ),
+    );
+  }
+}
+
+class BykakStory extends StatelessWidget {
+  const BykakStory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+      ),
+      child: Center(
+        child: SizedBox(
+          width: 1400,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'By 覺 Story',
+                style: TextStyle(
+                  fontSize: 64,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 16,
+                ),
+                child: Text(
+                  '2014년 김주현바이각은 인천의 고급 수제양복을 알리기 위해,\n남성들을 위한 올바른 스타일링과 문화적 놀이터를 만들고자 설립되었습니다.',
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TailorShopScreen extends StatelessWidget {
+  const TailorShopScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+      ),
+      child: Center(
+        child: SizedBox(
+          width: 1400,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'k!mjuhyeon by 覺',
+                style: TextStyle(
+                  fontSize: 56,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 40,
+                  bottom: 20,
+                ),
+                child: Text(
+                  'Lorem Ipsum is simply dummy text of the printing and typesetting industry.\nLorem Ipsum has been the industrys standard dummy text ever since the 1500s',
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 40,
+                ),
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    '자세히보기',
+                    style: TextStyle(
+                      fontSize: 20,
+                      decoration: TextDecoration.underline,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 640,
+                    height: 360,
+                    color: Colors.black,
+                  ),
+                  Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '총 10,000,000 벌 제작',
+                          style: TextStyle(
+                            fontSize: 32,
+                          ),
+                        ),
+                        Text(
+                          '총 100,000,000 번의 바느질',
+                          style: TextStyle(
+                            fontSize: 32,
+                          ),
+                        ),
+                        Text(
+                          '총 1,000,000,000 시간의 제작시간',
+                          style: TextStyle(
+                            fontSize: 32,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
